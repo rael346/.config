e9c75e5777995e300332cca84a883ef9d54e895f
Duy Tran <duyvutran2303@gmail.com>
2026-08-04 16:33:57 -0400

    fix: fix vim-zellij-navigator not loading in zellij

--- nvim/plugin/misc.lua
+++ nvim/plugin/misc.lua
@@ -13,15 +13,26 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
 vim.pack.add({
   "https://github.com/mrjones2014/smart-splits.nvim",
-  "https://github.com/chomosuke/typst-preview.nvim",
 })
 local smart_split = require("smart-splits")
-smart_split.setup()
+smart_split.setup({
+  zellij_move_focus_or_tab = true,
+  multiplexer_integration = "zellij",
+  log_level = "debug",
+})
 vim.keymap.set("n", "<C-h>", smart_split.move_cursor_left)
 vim.keymap.set("n", "<C-j>", smart_split.move_cursor_down)
 vim.keymap.set("n", "<C-k>", smart_split.move_cursor_up)
 vim.keymap.set("n", "<C-l>", smart_split.move_cursor_right)
 
+vim.keymap.set("n", "<C-S-h>", smart_split.resize_left)
+vim.keymap.set("n", "<C-S-j>", smart_split.resize_down)
+vim.keymap.set("n", "<C-S-k>", smart_split.resize_up)
+vim.keymap.set("n", "<C-S-l>", smart_split.resize_right)
+
+vim.pack.add({
+  "https://github.com/chomosuke/typst-preview.nvim",
+})
 require("typst-preview").setup({
   tinymist = "/opt/homebrew/bin/tinymist",
 })
